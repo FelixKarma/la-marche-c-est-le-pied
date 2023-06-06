28eda441276077f621aabcd0eaf96db4d50a8500
Felix Karma <romain_wyon@orange.fr>
2023-06-06 11:36:16 +0200

    modification header

--- src/views/Accueil.jsx
+++ src/views/Accueil.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import Bouton from '../components/utils-components/Bouton'
 import '../assets/css/Accueil.css'
+import { NavLink } from 'react-router-dom';
 
 const Acceuil = () => {
 
   useEffect(() => {
-    document.body.setAttribute('class','overflow-hidden');
+    document.body.setAttribute('class', 'overflow-hidden');
     return () => {
       document.body.removeAttribute('class');
     };
@@ -13,12 +14,14 @@ const Acceuil = () => {
 
   return (
     <div id='accueil'>
-      <figure className='position-absolute top-50 start-50 translate-middle' >
+      <figure className='position-absolute top-50 start-50 translate-middle d-flex' >
         {/* TODO: Mettre l'url correspondante */}
         <a href="http://" target="_blank">
           <Bouton text="S'inscrire" className="bg-green text-white fs-4 me-5" />
         </a>
-        <Bouton text="Voir les parcours" className="btn-primary text-white fs-4 ms-5" />
+        <NavLink to="/parcours" className="nav-link">
+          <Bouton text="Voir les parcours" className="btn-primary text-white fs-4 ms-5" />
+        </NavLink>
       </figure>
     </div>
   )
